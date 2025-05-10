6a3cfd87c4bad8f74e60eb9e14fca7b646e8408f
Pablo Saelices <psaelices@uoc.edu>
2025-05-10 23:00:52 +0100

    Full stage scene prepared with integrated animations

---
+++ Scripts/Game/stage.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+func _ready():
+	$Torch_Flame_Left.play("idle")
+	$Torch_Flame_Right.play("idle")
+	$Gear_Celling_Left.play("idle")
+	$Gear_Celling_Right.play("idle")
+	$Half_Gear_Left.play("idle")
+	$Half_Gear_Right.play("idle")
